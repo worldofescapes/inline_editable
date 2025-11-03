001a6c18051325b628dfdca9664b9286dee1efff
Alan <Alan-eng@yandex.ru>
2025-11-03 20:43:12 +0300

    убрал Active Admin из engine

--- lib/inline_editable.rb
+++ lib/inline_editable.rb
@@ -2,7 +2,7 @@ require 'inline_editable/version'
 require 'inline_editable/engine'
 
 module InlineEditable
-  # Пустой модуль, все функциональность определена в Helper
+  def self.version
+    VERSION
+  end
 end
-
-require_relative '../app/helpers/inline_editable/helper'
--- lib/inline_editable/engine.rb
+++ lib/inline_editable/engine.rb
@@ -1,15 +1,9 @@
 require 'rails/engine'
 
 module InlineEditable
-  class Engine < ::Rails::Engine
-    isolate_namespace InlineEditable
-
-    config.to_prepare do
-      if defined?(ActiveAdmin)
-        ActiveAdmin::BaseController.send :include, InlineEditable::Helper
-        ActiveAdmin::Views::Pages::Base.send :include, InlineEditable::Helper
-        ActiveAdmin::Views::IndexAsTable.send :include, InlineEditable::Helper
-      end
+  class Engine < Rails::Engine
+    initializer 'inline_editable' do
+      ActiveSupport.on_load(:action_view) { include InlineEditable::Helper }
     end
   end
 end
